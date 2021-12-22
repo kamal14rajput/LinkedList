1176d6d48451256810e6c3ab8368fc5448bdfd45
kamal14rajput <kamal14rajput@gmail.com>
2021-12-23 00:42:59 +0530

    UPDATED: appended linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,11 +5,18 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		MyLinkedList<Integer> mylinkedlist = new MyLinkedList<Integer>();
 		
+		System.out.println("Using add method: ");
 		mylinkedlist.add(70);
 		mylinkedlist.add(30);
 		mylinkedlist.add(56);
 		
 		mylinkedlist.display();
+		
+		System.out.println("\nUsing append method");
+		mylinkedlist.append(56);
+		mylinkedlist.append(30);
+		mylinkedlist.append(70);
+		mylinkedlist.display();	
 	}
 
 }
